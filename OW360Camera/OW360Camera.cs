28978a27478ef7ed3333345fc5570baf0deabc5c
Noah Pilarski <mehmehmeh313@gmail.com>
2022-04-22 23:33:36 -0400

    Fix DirectoryNotFoundException

--- OW360Camera/OW360Camera.cs
+++ OW360Camera/OW360Camera.cs
@@ -106,7 +106,10 @@ namespace OW360Camera
             equirect.Release();
 
             byte[] bytes = screenshot.EncodeToPNG();
-            string path = ModHelper.Config.GetSettingsValue<string>("SavePath") + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".png";
+            string directory = ModHelper.Config.GetSettingsValue<string>("SavePath");
+            string path = directory + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".png";
+            if (!Directory.Exists(directory))
+                Directory.CreateDirectory(directory);
             File.WriteAllBytes(path, bytes);
             
             ModHelper.Console.WriteLine("Saved ScreenShot to " + path, MessageType.Success);
